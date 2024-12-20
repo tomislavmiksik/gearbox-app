0576a1628c61c9d5496b778d16abd8a717376175
Tomislav Miksik <tomislav.miksik@gmail.com>
2024-12-20 11:45:00 +0100

    refactor: complete Koin DI setup
    Update application class and DI modules to use Koin instead of Hilt. Implement proper module definitions and dependency injection configuration.

--- app/src/main/java/de/comsystoreply/gearbox/GearboxApp.kt
+++ app/src/main/java/de/comsystoreply/gearbox/GearboxApp.kt
@@ -1,14 +1,22 @@
 package de.comsystoreply.gearbox
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import de.comsystoreply.gearbox.di.appModule
+import de.comsystoreply.gearbox.di.networkModule
+import de.comsystoreply.gearbox.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 
-@HiltAndroidApp
 class GearboxApp : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        
+        startKoin {
+            androidContext(this@GearboxApp)
+            modules(appModule, networkModule, viewModelModule)
+        }
     }
 }
--- app/src/main/java/de/comsystoreply/gearbox/di/AppModule.kt
+++ app/src/main/java/de/comsystoreply/gearbox/di/AppModule.kt
@@ -1,57 +1,52 @@
 package de.comsystoreply.gearbox.di
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStoreFile
 import com.google.gson.Gson
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import de.comsystoreply.gearbox.domain.services.ApiService
+import de.comsystoreply.gearbox.features.home.viewmodel.HomeViewModel
 import de.comsystoreply.gearbox.util.Constants.AUTH_PREFERENCES
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-
-    @Provides
-    @Singleton
-    fun providePreferenceDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+val appModule = module {
+    single<DataStore<Preferences>> {
         PreferenceDataStoreFactory.create(
             produceFile = {
-                context.preferencesDataStoreFile(AUTH_PREFERENCES)
+                androidContext().preferencesDataStoreFile(AUTH_PREFERENCES)
             }
         )
+    }
 }
 
-@Module
-@InstallIn(SingletonComponent::class)
-object RetrofitModule {
-    @Provides
-    fun provideBaseUrl(): String = "http://10.0.2.2:8080/"
+val networkModule = module {
+    single<String>(qualifier = named("baseUrl")) {
+        "http://10.0.2.2:8080/"
+    }
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(
-            GsonConverterFactory.create(
-                Gson().newBuilder()
-                    .setLenient()
-                    .create()
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(get<String>(qualifier = named("baseUrl")))
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    Gson().newBuilder()
+                        .setLenient()
+                        .create()
+                )
             )
-        )
-        .build()
+            .build()
+    }
 
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiService = retrofit.create(ApiService::class.java)
+    single<ApiService> {
+        get<Retrofit>().create(ApiService::class.java)
+    }
+}
 
+val viewModelModule = module {
+    factory { HomeViewModel(get()) }
 }
