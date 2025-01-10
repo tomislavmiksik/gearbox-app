29e3fb5ad7753f26d547c4869a558f7ecb04de6e
Tomislav Miksik <tomislav.miksik@gmail.com>
2025-01-10 14:00:00 +0100

    refactor: implement edge-to-edge display architecture
    Remove unnecessary Scaffold wrapper from MainActivity to support proper edge-to-edge display and prevent system UI conflicts.

--- app/src/main/java/de/comsystoreply/gearbox/MainActivity.kt
+++ app/src/main/java/de/comsystoreply/gearbox/MainActivity.kt
@@ -6,23 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import dagger.hilt.android.AndroidEntryPoint
-import de.comsystoreply.gearbox.navigation.RootNavHost
+import de.comsystoreply.gearbox.navigation.GearboxNavigation
 import de.comsystoreply.gearbox.ui.theme.GearboxTheme
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             GearboxTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
-                    RootNavHost()
-                }
+                GearboxNavigation()
             }
         }
     }
